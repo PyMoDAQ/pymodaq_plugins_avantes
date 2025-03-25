7ad0742924c376d007fda69789108a9c1e71be6f
Bernhard Lang <bernhard.lang@unige.ch>
2025-03-25 11:53:59 +0100

    fixed bug in parameter names for digital lines

--- src/pymodaq_plugins_avantes/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Avantes.py
+++ src/pymodaq_plugins_avantes/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Avantes.py
@@ -23,7 +23,7 @@ class DAQ_1DViewer_Avantes(DAQ_Viewer_base):
         {'title': 'Integration time:', 'name': 'integration_time',
          'type': 'float', 'min': 0.001, 'value': 0.1,
          'tip': 'Integration time in seconds'},
-    ] + [ {'title': 'Output %d:' % (i + 1), 'name': 'output %d' % (i + 1),
+    ] + [ {'title': 'Output %d:' % (i + 1), 'name': 'output_%d' % (i + 1),
            'type': 'led_push', 'value': False,
            'tip': 'Logic level on putput %d' % (i + 1) } \
           for i in range(10)
